877605cbca0088d42e680d4f574bf43caf08840e
Pushergene <pushergene@protonmail.com>
2020-06-10 13:13:31 +0200

    Update newfile.py

--- newfile.py
+++ newfile.py
@@ -1,12 +1,11 @@
 def ProzentwertRechnen():
-	Kapital = float(input("Geben Sie Grundwert an: "))
-	Prozentsatz = float(input("Geben Sie Prozentsatz an: "))
-	Zw = float(Prozentsatz * Grundwert)
-	if zinsen == "ja":
-		for l in range(0,12):
-			l += 1
-			Zinswert = Zw / 12 * l
-			print(l, Zinswert)
+  Kapital = float(input("Geben Sie Grundwert an: "))
+  Prozentsatz = float(input("Geben Sie Prozentsatz an: "))
+  Zw = float(Prozentsatz * Kapital // 100)
+  for l in range(0,12):
+    l += 1
+    Zinswert = (Zw / 12 * l)
+    print(l, Zinswert)
 
 while True:
     ProzentwertRechnen()
