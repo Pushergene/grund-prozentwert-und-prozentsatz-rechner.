d2820b12bc9e9db6576d53db6fa3b4762fcf117f
Pushergene <pushergene@protonmail.com>
2020-06-19 01:46:26 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,25 +1,19 @@
 #include <iostream>
 using namespace std;
-class Hallo {
-    public:
-    void Zinsrechnen() {
-        int Prozentsatz;
-        int Kapital;
-        Proz = Prozentsatz;
-        Kap = Kapital;
-        cin >> Prozentsatz;
-        cin >> Kapital;
-        int x = Prozentsatz * Kapital / 100;
-        for(int y = 0; y < 12; y*=1) {
-            cout << x << "\n";
-        }
-    }
-    private:
-    int Proz;
-    int Kap;
-};
 
-int main() {
-    Hallo h;
-    h.Zinsrechnen();
+class Grundwert {
+    private:
+    short int prozentsatzprivat;
+    short int prozentwertprivat;
+    public:
+    Grundwert(int prozentsatz, int prozentwert);
+}
+Grundwert::Grundwert(short int prozentsatz, short int prozentwert)
+    : prozentsatprivat(prozentsatz), prozentwertprivat(prozentwert) {
+    short int gw; 
+    std::cout << "Geben Sie Prozentsatz an." << endl;
+    std::cin >> prozentsatz;
+    std::cout << "Geben Sie Prozentwert an." << endl;
+    std::cin >> prozentwert;
+    
 }
